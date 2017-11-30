07190a244a22f6c35a8e5af107d7f68a58c7d444
dusan <ghostkingston@gmail.com>
2017-11-30 19:23:43 +0800

    绘制长方形---20171130

---
+++ 6-11.py
@@ -0,0 +1,13 @@
+# -*-coding:utf-8-*-
+# 以特定符号<元素>（！ *等）绘制长a，宽b的长方形
+def rect(a, b, fh):
+    for i in range (1, b + 1):
+        for j in range (1, a + 1):
+            print u'%s' % fh,
+        print u'\n'
+
+
+a = int (input (u'输入长方形的长度（整数）：'))
+b = int (input (u'输入长方形的宽度（整数）：'))
+fh = raw_input (u'输入长方形的元素（符号）：')
+rect (a, b, fh)
