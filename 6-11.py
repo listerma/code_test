c94362958978572f026d6c989596fa7aa6682ee1
dusan <ghostkingston@gmail.com>
2017-11-30 20:20:24 +0800

    py 3---20171130

--- 6-11.py
+++ 6-11.py
@@ -3,11 +3,11 @@
 def rect(a, b, fh):
     for i in range (1, b + 1):
         for j in range (1, a + 1):
-            print u'%s' % fh,
-        print u'\n'
+            print (u'%s' % fh, end="")
+        print (u'\n')
 
 
 a = int (input (u'输入长方形的长度（整数）：'))
 b = int (input (u'输入长方形的宽度（整数）：'))
-fh = raw_input (u'输入长方形的元素（符号）：')
+fh = str (input (u'输入长方形的元素（符号）：'))
 rect (a, b, fh)
